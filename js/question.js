4768c314af8e8dc988cbb0f1eed1548151bf392d
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-02 10:42:16 -0500

    Agregando el banco de preguntas

---
+++ js/question.js
@@ -0,0 +1,148 @@
+const math = {
+    1: {
+      question: "¿Qué cantidad expresa el número romano V?",
+      options: ["3", "2", "8", "5"],
+      correct: "5",
+    },
+    2: {
+      question: "¿Cómo se llama el polígono de siete lados?",
+      options: ["Heptágono", "Hexágono", "Duocágono", "Octágono"],
+      correct: "Heptágono",
+    },
+    3: {
+      question: "¿Cuántos minutos tiene una hora?",
+      options: ["50", "60", "24", "100"],
+      correct: "60",
+    },
+    4: {
+      question: "¿Cuánto es 12-7?",
+      options: ["6", "4", "5", "7"],
+      correct: "5",
+    },
+    5: {
+      question: "¿Cuál es el resultado de multiplicar 2x10?",
+      options: ["12", "120", "20", "200"],
+      correct: "20",
+    }
+  }
+  
+  const biology = {
+    1: {
+      question: "¿Dónde realizan las plantas la fotosíntesis?",
+      options: ["En el aire", "En la raiz", "En las hojas", "En el suelo"],
+      correct: "En las hojas",
+    },
+    2: {
+      question: "¿Qué músculo impulsa la sangre por todo nuestro cuerpo?",
+      options: ["Las manos", "El corazón", "La lengua", "Las piernas"],
+      correct: "El corazón",
+    },
+    3: {
+      question: "¿Un animal cuadrupedo cuantas patas tiene?",
+      options: ["3", "1", "4", "8"],
+      correct: "4",
+    },
+    4: {
+      question: "¿Por dónde se transportan los nutrientes en los humanos?",
+      options: ["El aire", "El suelo", "La boca", "Vasos sanguíneos"],
+      correct: "Vasos sanguíneos",
+    },
+    5: {
+      question: "¿Qué comen los carnivoros?",
+      options: ["Aire", "Carne", "Vegetales", "Pasto"],
+      correct: "Carne",
+    }
+  }
+  
+  const spanish = {
+    1: {
+      question: "¿Qué tipo de palabra es murciélago?",
+      options: ["Aguda", "Esdrújula", "Grave", "Sobreesdrújula"],
+      correct: "Esdrújula"
+    },
+    2: {
+      question: "Elija un sinónimo de feliz",
+      options: ["Triste", "Contento", "Aburrido", "Deprimido"],
+      correct: "Contento"
+    },
+    3: {
+      question: "Elija un antónimo de feliz",
+      options: ["Triste", "Contento", "Alegre", "Gozoso"],
+      correct: "Triste"
+    },
+    4: {
+      question: "¿Quién escribió el Quijote?",
+      options: ["Marqués Salamanca", "Marqués Santillana", "Juan Castillo", "Miguel de Cervantes"],
+      correct: "Miguel de Cervantes"
+    },
+    5: {
+      question: "¿Cuáles fueron los primeros textos escritos en castellano y árabe?",
+      options: ["Romances", "Jarchas", "Glosas", "Prosas"],
+      correct: "Jarchas"
+    }
+  }
+  
+  const tech = {
+    1: {
+      question: "¿Qué tecnología se utiliza para hacer posibles las llamadas telefónicas a través de Internet?",
+      options: ["Ethernet", "Bluetooth", "POP", "VoIP"],
+      correct: "VoIP",
+    },
+    2: {
+      question: "¿Qué lenguaje informático es el más utilizado?",
+      options: ["PHP", "C", "Java", "SQL"],
+      correct: "Java",
+    },
+    3: {
+      question: "¿Quién inventó Google?",
+      options: ["Steve Jobs", "Bill Gates", "Steve wozniak", "Larry Page"],
+      correct: "Larry Page",
+    },
+    4: {
+      question: "¿Cuándo se fundó Nintendo?",
+      options: ["1896", "1889", "1902", "1870"],
+      correct: "1889",
+    },
+    5: {
+      question: "¿Cuál es la página de Internet más visitada del mundo?",
+      options: ["Google", "Facebook", "Gmail", "Twitter"],
+      correct: "Google",
+    }
+  }
+  
+  const culture = {
+    1: {
+      question: "¿Qué país tiene más islas en el mundo?",
+      options: ["Suiza", "Noruega", "Suecia", "Finlandia"],
+      correct: "Suecia"
+    },
+    2: {
+      question: "¿En qué año murió John F. Kennedy?",
+      options: ["1963", "1964", "1960", "1961"],
+      correct: "1963"
+    },
+    3: {
+      question: "En la mitología griega, ¿quién era el mensajero de los dioses?",
+      options: ["Hermes", "Hefesto", "Hestia", "Apolo"],
+      correct: "Hermes"
+    },
+    4: {
+      question: "¿En qué año llegó Cristóbal Colón a América?",
+      options: ["1352", "1492", "1485", "1396"],
+      correct: "1492"
+    },
+    5: {
+      question: "¿Quién es el autor de la obra la La Virgen de las rocas?",
+      options: ["Jan Van Eyck", "Caravaggio", "Leonardo Da Vinci", "Edouard Manet"],
+      correct: "Leonardo Da Vinci"
+    }
+  }
+  
+  
+  export {
+    math,
+    spanish,
+    culture,
+    biology,
+    tech,
+  }
